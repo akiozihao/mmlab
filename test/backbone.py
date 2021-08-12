d0266f1ff16d80557e97e5d7f2f77eb4f1195bae
LesterLian <lianzizhen@gmail.com>
2021-08-12 00:20:38 -0400

    Test: backbone

--- test/backbone.py
+++ test/backbone.py
@@ -1,13 +1,28 @@
 import torch
 from mmdet.models.backbones.dla import DLA
 
-crowdhuman_path = '/home/akio/Downloads/new_crowdhuman.pth'
+from CenterTrack.src.lib.model.networks.dla import BasicBlock, DLA as DLA_Ori
 
-st = torch.load(crowdhuman_path)['state_dict']
+backbone_path = '../tensors/backbone.pt'
+# backbone_path = '/home/akio/Downloads/new_crowdhuman.pth'
+
 
 backbone = DLA(levels=[1, 1, 1, 2, 2, 1],
                channels=[16, 32, 64, 128, 256, 512])
+backbone_ori = DLA_Ori([1, 1, 1, 2, 2, 1],
+                       [16, 32, 64, 128, 256, 512],
+                       block=BasicBlock)
+st = torch.load(backbone_path)
 
+backbone.load_state_dict(st)
+backbone_ori.load_state_dict(st)
+assert all([(v1 == v2).all() for v1, v2 in zip(backbone.parameters(), backbone_ori.parameters())])
+x = torch.randn(1, 3, 544, 960)
+pre_img = torch.randn(1, 3, 544, 960)
+pre_hm = torch.randn(1, 1, 544, 960)
 
-for k,v in backbone.state_dict().items():
-    print(k,v.shape)
+output1 = backbone(x, pre_img, pre_hm)
+
+output2 = backbone_ori(x, pre_img, pre_hm)
+assert all([(v1 == v2).all() for v1, v2 in zip(output1, output2)])
+
