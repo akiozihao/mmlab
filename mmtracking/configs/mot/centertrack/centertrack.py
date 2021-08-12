4ff625e30f92b82a4c243b6cabb3248a44681d3e
akiozihao <akiozihaoceat@gmail.com>
2021-08-12 20:25:06 +0800

    update config

--- mmtracking/configs/mot/centertrack/centertrack.py
+++ mmtracking/configs/mot/centertrack/centertrack.py
@@ -1,6 +1,7 @@
 custom_imports = dict(
     imports=['mmdet.models.backbones.dla',
-             'mmdet.models.neck.dla_neck'],
+             'mmdet.models.neck.dla_neck',
+             'mmdet.models.dense_heads.centertrack_head'],
     allow_failed_imports=False
 )
 
@@ -16,6 +17,5 @@ model = dict(
         neck=dict(
             type='DLANeck',
             arch=34),
-
     )
 )
