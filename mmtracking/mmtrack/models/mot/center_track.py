78c477f85c613f7d50ed4a96fd76fc9577425d96
akiozihao <akiozihaoceat@gmail.com>
2021-08-14 22:03:46 +0800

    add center_track,ct_tracker

---
+++ mmtracking/mmtrack/models/mot/center_track.py
@@ -0,0 +1,146 @@
+import torch
+from mmdet.core import bbox2result
+from mmdet.models import build_detector
+from mmtrack.core import track2result
+
+from .base import BaseMultiObjectTracker
+from ..builder import MODELS, build_tracker
+
+
+@MODELS.register_module()
+class CenterTrack(BaseMultiObjectTracker):
+    def __init__(self,
+                 detector=None,
+                 tracker=None,
+                 pretrains=None,
+                 pre_thresh=0.5,
+                 use_pre_hm=True,
+                 ):
+        super(CenterTrack, self).__init__()
+        if detector is not None:
+            self.detector = build_detector(detector)
+
+        if tracker is not None:
+            self.tracker = build_tracker(tracker)
+
+        self.init_weights(pretrains)
+        # self.init_module('detector', pretrain.get('detector', False))  # todo
+        self.pre_thresh = pre_thresh
+        self.use_pre_hm = use_pre_hm
+
+        self.pre_img = None
+        self.pre_hm = None
+        self.pre_bboxes = None
+
+    def init_weights(self, pretrain):
+        """Initialize the weights of the modules.
+
+        Args:
+            pretrained (dict): Path to pre-trained weights.
+        """
+        if pretrain is None:
+            pretrain = dict()
+        assert isinstance(pretrain, dict), '`pretrain` must be a dict.'
+        if self.with_detector and pretrain.get('detector', False):
+            self.init_module('detector', pretrain['detector'])
+
+    def simple_test(self,
+                    img,
+                    img_metas,
+                    rescale=False,
+                    public_bboxes=None,  # todo check
+                    **kwargs):
+        """Test without augmentations.
+
+        Args:
+            img (Tensor): of shape (N, C, H, W) encoding input images.
+                Typically these should be mean centered and std scaled.
+            img_metas (list[dict]): list of image info dict where each dict
+                has: 'img_shape', 'scale_factor', 'flip', and may also contain
+                'filename', 'ori_shape', 'pad_shape', and 'img_norm_cfg'.
+            rescale (bool, optional): If False, then returned bboxes and masks
+                will fit the scale of img, otherwise, returned bboxes and masks
+                will fit the scale of original image shape. Defaults to False.
+            public_bboxes (list[Tensor], optional): Public bounding boxes from
+                the benchmark. Defaults to None.
+
+        Returns:
+            dict[str : list(ndarray)]: The tracking results.
+        """
+        frame_id = img_metas[0]['frame_id']
+        self.pre_bboxes_input = self.tracker.bboxes_input
+        if self.pre_bboxes_input is not None:
+            self.pre_bboxes_input = self.pre_bboxes_input[self.pre_bboxes_input[:, -1] > self.pre_thresh]
+        if frame_id == 0:
+            self.tracker.reset()
+            self.ref_img = img.clone()
+            if self.use_pre_hm:
+                n, c, h, w = img.shape
+                self.ref_hm = torch.zeros((n, 1, h, w), dtype=img.dtype, device=img.device)
+            else:
+                self.ref_hm = None
+                self.ref_img = None
+        else:
+            if self.use_pre_hm:
+                if self.pre_bboxes_input is None or self.pre_bboxes_input.shape[0] == 0:
+                    n, c, h, w = img.shape
+                    self.ref_hm = torch.zeros((n, 1, h, w), dtype=img.dtype, device=img.device)
+                else:
+                    self.ref_hm = self.detector._build_test_hm(self.ref_img, self.pre_bboxes_input)
+            else:
+                self.ref_hm = None
+                self.ref_img = None
+
+        # todo check this
+        batch_input_shape = tuple(img[0].size()[-2:])
+        img_metas[0]['batch_input_shape'] = batch_input_shape
+        x = self.detector.extract_feat(img, self.ref_img, self.ref_hm)
+        center_heatmap_pred, wh_pred, offset_pred, tracking_pred, ltrb_amodal_pred = self.detector.bbox_head(x)
+        outs = [center_heatmap_pred, wh_pred, offset_pred, tracking_pred, ltrb_amodal_pred]
+        result_list = self.detector.bbox_head.get_bboxes(
+            # todo Are outs always tensors?
+            *[[tensor] for tensor in outs], img_metas=img_metas, rescale=rescale)
+        # TODO: support batch inference
+        det_bboxes = result_list[0][0]
+        det_labels = result_list[0][1]
+        det_bboxes_with_motion = result_list[0][2]
+        det_bboxes_input = result_list[0][3]
+        num_classes = self.detector.bbox_head.num_classes
+        self.ref_img = img
+        bboxes, labels, ids = self.tracker.track(
+            img=img,
+            img_metas=img_metas,
+            bboxes_input=det_bboxes_input,
+            bboxes=det_bboxes,
+            bboxes_with_motion=det_bboxes_with_motion,
+            labels=det_labels,
+            frame_id=frame_id,
+            rescale=rescale,
+            **kwargs)
+        track_result = track2result(bboxes, labels, ids, num_classes)
+        bbox_result = bbox2result(det_bboxes, det_labels, num_classes)
+        return dict(bbox_results=bbox_result, track_results=track_result)
+
+    def forward_train(self,
+                      img,
+                      img_metas,
+                      gt_bboxes,
+                      gt_labels,
+                      gt_instance_ids,
+                      gt_match_indices,
+                      ref_img_metas,
+                      ref_img,
+                      ref_gt_bboxes,
+                      ref_gt_labels,
+                      ref_gt_match_indices,
+                      ref_gt_instance_ids):
+        return self.detector.forward_train(img,
+                                           img_metas,
+                                           gt_bboxes,
+                                           gt_labels,
+                                           gt_match_indices,
+                                           ref_img_metas,
+                                           ref_img,
+                                           ref_gt_bboxes,
+                                           ref_gt_labels,
+                                           ref_gt_match_indices)
---
+++ mmtracking/mmtrack/models/mot/trackers/ct_tracker.py
@@ -0,0 +1,125 @@
+import numpy as np
+import torch
+
+from mmtrack.models import TRACKERS
+from .base_tracker import BaseTracker
+
+
+@TRACKERS.register_module()
+class CTTracker(BaseTracker):
+    def __init__(self,
+                 obj_score_thr=0.4,
+                 momentums=None,
+                 num_frames_retain=1):
+        super(CTTracker, self).__init__(momentums, num_frames_retain)
+        self.obj_score_thr = obj_score_thr
+
+    def track(self,
+              img,
+              img_metas,
+              bboxes_input,
+              bboxes,
+              bboxes_with_motion,
+              labels,
+              frame_id,
+              rescale,
+              **kwargs):
+        valid_inds = bboxes[:, -1] > self.obj_score_thr
+        bboxes_input = bboxes_input[valid_inds]
+        bboxes = bboxes[valid_inds]
+        bboxes_with_motion = bboxes_with_motion[valid_inds]
+        labels = labels[valid_inds]
+
+        pre_bboxes = self.pre_bboxes
+        if self.empty or bboxes.size(0) == 0 or pre_bboxes is None:
+            num_new_tracks = bboxes.size(0)
+            ids = torch.arange(
+                self.num_tracks,
+                self.num_tracks + num_new_tracks,
+                dtype=torch.long)
+            self.num_tracks += num_new_tracks
+        else:
+            ids = torch.full((bboxes.size(0),), -1, dtype=torch.long)
+            M = pre_bboxes.shape[0]
+            N = bboxes.shape[0]
+            track_size = (pre_bboxes[:, 3] - pre_bboxes[:, 1]) * \
+                         (pre_bboxes[:, 2] - pre_bboxes[:, 0])  # M
+            item_size = (bboxes[:, 3] - bboxes[:, 1]) * (bboxes[:, 2] - bboxes[:, 0])  # N
+            det_centers_with_motion = self._xyxy2center(bboxes_with_motion)
+            # dist = torch.cdist(det_centers, self.pre_centers, 2)
+            dist = (((self.pre_cts.reshape(1, -1, 2) - \
+                      det_centers_with_motion.reshape(-1, 1, 2)) ** 2).sum(axis=2))
+            # invalid
+            invalid = ((dist > track_size.reshape(1, M)) + \
+                       (dist > item_size.reshape(N, 1)) + \
+                       (labels.reshape(N, 1) != self.pre_labels.reshape(1, M))) > 0
+            dist = dist + invalid * 1e18
+            matched_indices = self._greedy_assignment(dist)
+            # pre_ids = self.pre_ids
+            ids[matched_indices[:, 0]] = self.pre_ids[matched_indices[:, 1]]
+            new_track_inds = ids == -1
+            ids[new_track_inds] = torch.arange(
+                self.num_tracks,
+                self.num_tracks + new_track_inds.sum(),
+                dtype=torch.long)
+            self.num_tracks += new_track_inds.sum()
+        self.update(
+            ids=ids,
+            bboxes_input=bboxes_input,
+            bboxes=bboxes,
+            cts=self._xyxy2center(bboxes),
+            labels=labels,
+            frame_ids=frame_id)
+        return bboxes, labels, ids
+
+    @property
+    def bboxes_input(self):
+        bboxes = [track['bboxes_input'][-1] for id, track in self.tracks.items()]
+        if len(bboxes) == 0:
+            return None
+        return torch.cat(bboxes, 0)
+
+    @property
+    def pre_bboxes(self):
+        bboxes = [track['bboxes'][-1] for id, track in self.tracks.items()]
+        if len(bboxes) == 0:
+            return None
+        return torch.cat(bboxes, 0)
+
+    @property
+    def pre_cts(self):
+        cts = [track['cts'][-1] for id, track in self.tracks.items()]
+        if len(cts) == 0:
+            return None
+        return torch.cat(cts, 0)
+
+    @property
+    def pre_labels(self):
+        labels = [track['labels'][-1] for id, track in self.tracks.items()]
+        if len(labels) == 0:
+            return None
+        return torch.cat(labels, 0)
+
+    @property
+    def pre_ids(self):
+        ids = [track['ids'][-1] for id, track in self.tracks.items()]
+        if len(ids) == 0:
+            return None
+        return torch.cat(ids, 0)
+
+    def _xyxy2center(self, bbox):  # shape (N,5)
+        ctx = bbox[:, 0] + (bbox[:, 2] - bbox[:, 0]) / 2
+        cty = bbox[:, 1] + (bbox[:, 3] - bbox[:, 1]) / 2
+        return torch.cat((ctx.reshape(-1, 1), cty.reshape(-1, 1)), 1)
+
+    def _greedy_assignment(self, dist):
+        dist = dist.cpu().numpy()
+        matched_indices = []
+        if dist.shape[1] == 0:
+            return np.array(matched_indices, np.int32).reshape(-1, 2)
+        for i in range(dist.shape[0]):
+            j = dist[i].argmin()
+            if dist[i][j] < 1e16:
+                dist[:, j] = 1e18
+                matched_indices.append([i, j])
+        return np.array(matched_indices, np.int32).reshape(-1, 2)
