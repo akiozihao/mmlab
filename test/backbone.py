049411c80a10e093978df46479bfbcb0511b8d78
LesterLian <lianzizhen@gmail.com>
2021-08-13 23:59:14 -0400

    Test: add cuda to backbone test

--- test/backbone.py
+++ test/backbone.py
@@ -2,16 +2,18 @@ import torch
 from mmdet.models.backbones.dla import DLA
 
 from CenterTrack.src.lib.model.networks.dla import BasicBlock, DLA as DLA_Ori
+from utils import Struct
 
 backbone_path = '../tensors/backbone.pt'
 # backbone_path = '/home/akio/Downloads/new_crowdhuman.pth'
-
+opt = Struct(pre_img=True, pre_hm=True)
+use_cuda = True
 
 backbone = DLA(levels=[1, 1, 1, 2, 2, 1],
                channels=[16, 32, 64, 128, 256, 512])
 backbone_ori = DLA_Ori([1, 1, 1, 2, 2, 1],
                        [16, 32, 64, 128, 256, 512],
-                       block=BasicBlock)
+                       block=BasicBlock, opt=opt)
 st = torch.load(backbone_path)
 
 backbone.load_state_dict(st)
@@ -21,8 +23,14 @@ x = torch.randn(1, 3, 544, 960)
 pre_img = torch.randn(1, 3, 544, 960)
 pre_hm = torch.randn(1, 1, 544, 960)
 
-output1 = backbone(x, pre_img, pre_hm)
+if use_cuda:
+    backbone = backbone.cuda()
+    backbone_ori = backbone_ori.cuda()
+    x = x.cuda()
+    pre_img = pre_img.cuda()
+    pre_hm = pre_hm.cuda()
 
+output1 = backbone(x, pre_img, pre_hm)
 output2 = backbone_ori(x, pre_img, pre_hm)
 assert all([(v1 == v2).all() for v1, v2 in zip(output1, output2)])
 
