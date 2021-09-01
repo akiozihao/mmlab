45b5e353eaadb484402d0f08f3551487e5de3074
akiozihao <akiozihaoceat@gmail.com>
2021-09-01 22:43:47 +0800

    * bug fix config centertrack private

--- mmtracking/configs/mot/centertrack/centertrack_dla34_dcnv2_70e_mot17_private.py
+++ mmtracking/configs/mot/centertrack/centertrack_dla34_dcnv2_70e_mot17_private.py
@@ -1,7 +1,8 @@
 _base_ = ['./centertrack_dla34_dcnv2_70e_mot17_private-half.py']
 data_root = '../data/mot17-frcnn/'
 data = dict(
-    train=dict(ann_file=data_root + 'annotations/train_cocoformat.json'),
+    train=dict(
+        dataset=dict(ann_file=data_root + 'annotations/train_cocoformat.json')),
     val=dict(ann_file=data_root + 'annotations/train_cocoformat.json'),
     test=dict(ann_file=data_root + f'annotations/train_cocoformat.json')
 )
