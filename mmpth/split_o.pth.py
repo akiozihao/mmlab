cb556f55e863aeb86bce4f15a09271ce20451f2c
akiozihao <akiozihaoceat@gmail.com>
2021-09-26 22:26:07 +0800

    * add trans pth

--- mmpth/split_o.pth.py
+++ mmpth/split_o.pth.py
@@ -12,24 +12,26 @@ def split_pth(source_pth):
     source_state_dict = source['state_dict']
     for k, v in source_state_dict.items():
         k = k.split('.')
-        type = k[1]
-        nk = '.'.join(k[2:])
-        if type == 'backbone':
+        type = k[0]
+        if type == 'base':
+            nk = '.'.join(k[1:])
             backbone_st[nk] = v
-        elif type == 'neck':
+        elif type in ['dla_up','ida_up']:
+            nk = '.'.join(k)
             neck_st[nk] = v
-        elif type == 'bbox_head':
+        elif type in ['hm','reg','wh','tracking','ltrb_amodal']:
+            nk = '.'.join(k)
             head_st[nk] = v
         else:
             raise TypeError('type error')
 
     # save
-    torch.save(backbone_st,'test_mm_backbone.pt')
-    torch.save(neck_st,'test_mm_neck.pt')
-    torch.save(head_st,'test_mm_head.pt')
+    torch.save(backbone_st,'test_o_backbone.pt')
+    torch.save(neck_st,'test_o_neck.pt')
+    torch.save(head_st,'test_o_head.pt')
 
 
-split_pth('../models/mmlab_old_dcn_old_radius_mot17_half.pth')
+split_pth('/home/akio/dev/centertrack_origin/models/mot17_half.pth')
 
 for k, v in backbone_st.items():
     print(k, v.shape)
