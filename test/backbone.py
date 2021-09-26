a4bf053c3244251216cdb940391556f1a9b25d7c
akiozihao <akiozihaoceat@gmail.com>
2021-09-26 22:35:35 +0800

    * update test

--- test/backbone.py
+++ test/backbone.py
@@ -1,21 +1,22 @@
+
 import torch
 from mmdet.models.backbones.dla import DLA
-from utils import Struct
+from test_utils import Struct
 
 from CenterTrack.src.lib.model.networks.dla import DLA as DLA_Ori
 from CenterTrack.src.lib.model.networks.dla import BasicBlock
 
-# backbone_path = '../tensors/backbone.pt'
-backbone_path_ori = '/home/akio/Downloads/crowdhuman_split/backbone.pt'
-backbone_path = '../mmpth/backbone.pt'
+backbone_path_ori = '../mmpth/test_o_backbone.pt'
+backbone_path = '../mmpth/test_mm_backbone.pt'
 opt = Struct(pre_img=True, pre_hm=True)
 use_cuda = True
 
-backbone = DLA(levels=[1, 1, 1, 2, 2, 1], channels=[16, 32, 64, 128, 256, 512])
+backbone = DLA(levels=[1, 1, 1, 2, 2, 1],
+               channels=[16, 32, 64, 128, 256,512],
+               norm_cfg=dict(type='BN', momentum=0.1))
 backbone_ori = DLA_Ori([1, 1, 1, 2, 2, 1], [16, 32, 64, 128, 256, 512],
                        block=BasicBlock,
                        opt=opt)
-
 st_ori = torch.load(backbone_path_ori)
 st = torch.load(backbone_path)
 
@@ -24,6 +25,7 @@ backbone_ori.load_state_dict(st_ori)
 assert all([(v1 == v2).all()
             for v1, v2 in zip(backbone.parameters(), backbone_ori.parameters())
             ])
+
 x = torch.randn(1, 3, 544, 960)
 pre_img = torch.randn(1, 3, 544, 960)
 pre_hm = torch.randn(1, 1, 544, 960)
